6011ba88500b1aea7a1b4426e1e6520cbd67e872
Adocly <adoclyanimais2024@gmail.com>
2024-10-15 16:40:34 -0300

    Atualização (15.10.2024)

--- js/login.js
+++ js/login.js
@@ -1,6 +1,6 @@
-const email = document.querySelector("#email")
-const senha = document.querySelector("#senha")
-const login = document.querySelector("#login")
+const email = document.getElementById("email")
+const senha = document.getElementById("senha")
+const login = document.getElementById("login")
 
 email.addEventListener("keyup", (e) => {
     const value = e.currentTarget.value;
