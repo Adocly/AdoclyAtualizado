f82ab2ed2be644d48fd50d71f4790609def00d32
Adocly <adoclyanimais2024@gmail.com>
2024-10-16 00:22:57 -0300

    Atualização (15.10.2024)

---
+++ js/cadastro.js
@@ -0,0 +1,26 @@
+/*const firebaseConfig = {
+  apiKey: "AIzaSyADjxwj3klBs7JmVYhXiAAXBr0m3D3qOSU",
+  authDomain: "teste-349f7.firebaseapp.com",
+  projectId: "teste-349f7",
+  storageBucket: "teste-349f7.appspot.com",
+  messagingSenderId: "157850767446",
+  appId: "1:157850767446:web:650798ee2761aeca73abf7"
+  };
+    
+    
+const app = initializeApp(firebaseConfig);
+
+function Cadatrar(){
+  var auth = null;
+
+  firebase.auth().createUserwithEmailAndPassword(document.getElementById("email").value, 
+  document.getElementById("senha").value).then(function (user){
+    alert("Cadastro feito com suceso!");
+    auth = user;
+
+    document.getElementById("email").value = ''
+    document.getElementById("senha").value = ''
+  }).cath(function(error){
+    alert("Falha ao se cadastrar")
+  })
+}*/
