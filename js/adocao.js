406ca4a8e88ed255def8ed45e8f4b86afcfaa3e5
Adocly <adoclyanimais2024@gmail.com>
2024-11-05 19:35:28 -0300

    Deploy

--- js/adocao.js
+++ js/adocao.js
@@ -1,3 +1,9 @@
+function menuOnClick() {
+    document.getElementById("menu-bar").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+  }
+
 function abrirModal(){
     const modal = document.getElementById('janelaModal');
     modal.classList.add('abrir');
