5f3cd3b9db6bd463ee44665017378122b22808e2
Adocly <adoclyanimais2024@gmail.com>
2024-11-26 21:10:29 -0300

    Atualizações (26.11.2024)

--- js/avaliacoes.js
+++ js/avaliacoes.js
@@ -15,16 +15,4 @@ function modalConta(){
     })
 };
 
-const nome = document.getElementById('nome').value;
-const email = document.getElementById('email').value;
-const mensagem = document.getElementById('mensagem').value;
-const btnEnviar = document.getElementById('btnEnviar');
-
-btnEnviar.addEventListener('click', function(){
-    if(nome === nome.value && email === email.value && mensagem === mensagem.value){
-        alert("Mensagem enviada com sucesso!")
-    }
-    else{
-        console.log("Erro ao mandar mensagem")
-    }
-})
+const enviado = document.getElementById('enviado');
