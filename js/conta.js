bfcd27f68614f57f8bfcccfd1c37a4921c2dc0fe
Adocly <adoclyanimais2024@gmail.com>
2024-11-30 23:10:58 -0300

    Configurando tela de conta. (Yasmim).

--- js/conta.js
+++ js/conta.js
@@ -4,14 +4,45 @@ function menuOnClick() {
     document.getElementById("menu-bg").classList.toggle("change-bg");
   }
 
-  document.getElementById('btnPerfil').addEventListener('click', () => {
-    const user = auth.currentUser;
+  
 
-    if (user) {
-        document.getElementById('emailConta').textContent = user.email;
-        alert(user.email); // Mover o alert para dentro
-    } else {
+  function logout(){
+    firebase.auth().signOut().then(() => {
+        alert("Conta deslogada com sucesso!")
+        window.location.href = "../index.html"
+        history.replaceState(null, "", "../index.html")
+                }).catch(() => {
+        alert("Erro ao sair da conta.")
+    })
+  }
+
+  const auth = firebase.auth();
+        document.getElementById('btnPerfil').addEventListener('click', () => {
+         const user = auth.currentUser;
+
+        if (user) {
+            document.getElementById('emailConta').textContent = user.email;
+        } else {
         document.getElementById('emailConta').textContent = 
             'Erro: Nenhum usuário logado.';
+        }
+            });
+
+
+
+const excluirConta = document.getElementById('excluirConta')
+
+excluirConta.addEventListener('click', () =>{
+    const user1 = auth.currentUser;
+    if(user1){
+        user1.delete()
+        .then(() => {
+          alert("Conta excluída com sucesso.");
+          window.location.href = "../index.html"
+          history.replaceState(null, "", "../index.html")
+        }).catch((error) => {
+            console.error("Erro ao excluir a conta:", error);
+        })
     }
-});
+})
+
